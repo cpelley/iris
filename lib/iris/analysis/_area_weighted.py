ef97661a3a4de33f9c3fd19eaf8d8db1bc637800
Carwyn Pelley <cpelley.pub@gmail.com>
2014-10-24 08:58:01 +0100

    Started on two-stage regrid

--- lib/iris/analysis/_area_weighted.py
+++ lib/iris/analysis/_area_weighted.py
@@ -20,6 +20,57 @@ import numpy as np
 from iris.analysis._interpolation import get_xy_dim_coords, snapshot_grid
 import iris
 import iris.experimental.regrid as eregrid
+from iris.analysis._linear import LinearRegridder
+
+
+def _gen_intermediate_target(cube, target_grid, shape):
+    # Generate a grid cube which covers the same area as the source cube only
+    # that it has the coordinate system of the target grid and specified shape.
+    assert cube.ndim == 2
+
+    dtype = cube.lazy_data().dtype
+    data = biggus.ConstantArray(shape, dtype=dtype)
+    grid_cube = iris.cube.Cube(data)
+
+    src_x_coord = cube.coord(axis='x')
+    src_x_points = (src_x_coord.bounds if src_x_coord.has_bounds() else
+                    src_x_coord.points)
+    src_y_coord = cube.coord(axis='y')
+    src_y_points = (src_y_coord.bounds if src_y_coord.has_bounds() else
+                    src_y_coord.points)
+
+    # Determine bounding box around source data.
+    src_x_points = np.array((src_x_points[0], src_x_points[-1],
+                             src_x_points[-1], src_x_points[0]))
+    src_y_points = np.array((src_y_points[0], src_y_points[0],
+                             src_y_points[-1], src_y_points[-1]))
+
+    src_crs = src_x_coord.coord_system.as_cartopy_crs()
+    tgt_crs = target_grid.coord(axis='x').coord_system.as_cartopy_crs()
+    xyz = tgt_crs.transform_points(src_crs, src_x_points, src_y_points)
+    x, y = xyz[..., 0], xyz[..., 1]
+
+    # Generate points by utilising points if available and if not, use points.
+    if src_x_coord.has_bounds():
+        x_bound = np.linspace(x.min(), x.max(), num=shape[1]+1)
+        grid_x = (x_bound + (x_bound[1] - x_bound[0])/2.)[:-1]
+    else:
+        grid_x = np.linspace(x.min(), x.max(), num=shape[1])
+    grid_x = grid_x.astype(dtype)
+
+    if src_y_coord.has_bounds():
+        y_bound = np.linspace(y.min(), y.max(), num=shape[0]+1)
+        grid_y = (y_bound + (y_bound[1] - y_bound[0])/2.)[:-1]
+    else:
+        grid_y = np.linspace(y.min(), y.max(), num=shape[0])
+    grid_y = grid_y.astype(dtype)
+
+    grid_x_coord = target_grid.coord(axis='x').copy(grid_x)
+    grid_y_coord = target_grid.coord(axis='y').copy(grid_y)
+    grid_cube.add_dim_coord(grid_y_coord, 0)
+    grid_cube.add_dim_coord(grid_x_coord, 1)
+
+    return grid_cube
 
 
 class AreaWeightedRegridder(object):
@@ -101,3 +152,51 @@ class AreaWeightedRegridder(object):
                              'source grid as this regridder.')
         return eregrid.regrid_area_weighted_rectilinear_src_and_grid(
             cube, self._target_grid_cube, mdtol=self._mdtol)
+
+
+class AproximatedAreaWeightedRegridder(AreaWeightedRegridder):
+    def __call__(self, cube):
+        """
+        Regrid this :class:`~iris.cube.Cube` onto the target grid of
+        this :class:`AreaWeightedRegridder`.
+
+        The given cube must be defined with the same grid as the source
+        grid used to create this :class:`AreaWeightedRegridder`.
+
+        Args:
+
+        * cube:
+            A :class:`~iris.cube.Cube` to be regridded.
+
+        Returns:
+            A cube defined with the horizontal dimensions of the target
+            and the other dimensions from this cube. The data values of
+            this cube will be converted to values on the new grid using
+            area-weighted regridding.
+
+        """
+        try:
+            return eregrid.regrid_area_weighted_rectilinear_src_and_grid(
+                cube, self._target_grid_cube, mdtol=self._mdtol)
+        except ValueError as err:
+            message = ('The horizontal grid coordinates of both the source and '
+                       'grid cubes must have the same coordinate system.')
+            if message in err.message:
+                warnings.warn(err.message)
+                warnings.warn('Two-stage regrid being performed, taking a '
+                              'bilinear interpolation to a similar resolution '
+                              'grid, then performing an area weighted '
+                              'regrid.'.format())
+                shape = cube.shape
+                intermediate_target = _gen_intermediate_target(cube,
+                                                               target_grid,
+                                                               shape)
+                intermediate_target.coord(axis='x').guess_bounds()
+                intermediate_target.coord(axis='y').guess_bounds()
+                linear = LinearRegridder(intermediate_target)
+                intermediate_target = linear(cube)
+                return eregrid.regrid_area_weighted_rectilinear_src_and_grid(
+                    intermediate_target, self._target_grid_cube,
+                    mdtol=self._mdtol)
+            else:
+                raise err
